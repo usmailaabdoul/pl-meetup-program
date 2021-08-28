19d2afea98b2b9f54e304240c650896ede8670c8
usmailaabdoul <ismaelabdul77@gmail.com>
2021-08-28 12:32:28 +0100

    Work for week 4, we looked at Arrays and their various methode we also looked at the task from the past weeek

---
+++ week4.js
@@ -0,0 +1,63 @@
+// ### ARRAYS AND ARRAY METHODS
+// - `find()`
+// - `findIndex()`
+// - `map()`
+// - `filter()`
+// - `indexOf()`
+// - `pop()`, shift() 
+// - `push() etc.`, unshift()
+// - `splice() etc.`
+
+// const newA = [];
+// const aa = [1,2,3,4,5];
+// const ab = aa.map((a) => { 
+//   return a*2
+// });
+
+
+// console.log({ab, aa});
+
+// arr.map((value) => {
+//   if(value.id === 4) {
+//     value.passed = true;
+//     newA.push(value);
+//   }
+// })
+// console.log({newA})
+// const a = [10,5,3,40];
+
+// const newUser = a.map((value) => {
+  // return arrUsers.find((a) => {
+  //   if (a.id === value) {
+  //     return a.email = 'ebot@gmail.com'
+  //   }
+  // })
+// })
+// let index1 = arrUsers.findIndex((a) => a.id === 3)
+// let index2 = arrUsers.findIndex((a) => a.id === 4)
+
+// if (index1 > -1 && index2 > -1) {
+//   arrUsers[index1].email = 'abdoul@gmail.com';
+//   arrUsers[index2].subscribed = true;
+// }
+// let removed = arrUsers.pop()
+// arrUsers.unshift(removed);
+// arrUsers.splice(index1, 2);
+// let newArr = arrUsers.slice(1, 3)
+
+
+// let val = arrUsers.filter((value) => value.name === 'ebot')
+// console.log({val})
+
+// ### LOOPS AND RECURSIVE LOOPS
+// const arrUsers = [
+//   {id: 1, name: 'abdoul'},
+//   {id: 2, name: 'usmaila'},
+//   {id: 3, name: 'ebot'},
+//   {id: 4, name: 'max'},
+// ]
+
+
+// ### Math.random(), parseInt(), stringify()
+
+// ### spreadoperators, destructuring, ternary operators
